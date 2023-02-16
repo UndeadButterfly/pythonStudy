4edd989838206cb2dd52d9ab8de82a1080540ad6
UndeadButterfly <knight2944731@gmail.com>
2023-02-16 14:44:19 +0900

    dropdown fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# name : aproject_db
+# user : root
+# password : 4321
+# host : localhost
+# port : 3306
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aproject_db',
+        'USER': 'root',
+        'PASSWORD': "",
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
